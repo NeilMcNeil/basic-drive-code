393fa47961826db72729e9f887e0604dab5c597f
NeilMcNeil <Neilmcneilrobotics@gmail.com>
2023-02-03 15:18:52 -0500

    initial

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,26 @@
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
+
+// package frc.robot;
+
+// import edu.wpi.first.wpilibj.Joystick;
+// import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+
+// /** Add your docs here. */
+// public class OI {
+//     Joystick driverStick = new Joystick(RobotMap.driverStickPort);
+//     Joystick operatorStick = new Joystick(RobotMap.operatorStickPort);
+//     JoystickButton speedDef = new JoystickButton(driverStick, 3);
+//     JoystickButton speed1 = new JoystickButton(driverStick, 1);
+//     JoystickButton speed2 = new JoystickButton(driverStick, 2);
+//     public Joystick getDriverStick(){
+//         return driverStick;
+//     }
+//     public Joystick getOperatorStick(){
+//         return operatorStick;
+//     }
+//     public OI(){
+//     }
+// }
